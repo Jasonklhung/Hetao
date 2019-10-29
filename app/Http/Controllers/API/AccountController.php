63f5c1f92292a5ec9bdacb8ac0ae42ac77375604
JasonHung <jasonklhung@accuhit.net>
2019-10-29 12:00:51 +0800

    accountStore api

--- app/Http/Controllers/API/AccountController.php
+++ app/Http/Controllers/API/AccountController.php
@@ -10,6 +10,14 @@ class AccountController extends Controller
 {
     public function store(Request $request)
     {
-    	dd($request->all());
+    	$account = new Account;
+    	$account->token = $request->userId;
+    	$account->cuskey = $request->CUSTKEY;
+    	$account->dept = $request->DEPT;
+    	$account->name = $request->name;
+    	$account->card_number = $request->CARDNO;
+    	$account->save();
+
+    	return response()->json(['success'=>['ok']]);
     }
 }

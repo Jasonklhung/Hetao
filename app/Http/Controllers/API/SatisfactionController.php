029f80b463f2268c639854a09925ff260b105eb1
JasonHung <jasonklhung@accuhit.net>
2019-10-29 19:05:21 +0800

    test

--- app/Http/Controllers/API/SatisfactionController.php
+++ app/Http/Controllers/API/SatisfactionController.php
@@ -21,65 +21,69 @@ class SatisfactionController extends Controller
     {
     	$form = array();
 
-    	foreach ($request->form as $key => $value) {
+    	$form[] = ['a'=>'1'];
+    	$form[] = ['b'=>'2'];
+    	$form[] = ['c'=>'3'];
 
-    		if(preg_match("/^radio+[0-9]+$/", $value->name)){ //radio
-    			foreach ($request->form as $k => $v) {
-    				if(preg_match("/^$value->name+Opt+$/", $v->name)){
+    	// foreach ($request->form as $key => $value) {
 
-    					$form[] = [$value->value=>$v->value];
-    				}
-    			}
-    		}
-    		elseif(preg_match("/^select+[0-9]+$/", $value->name)){ //select
-    			foreach ($request->form as $k => $v) {
-    				if(preg_match("/^$value->name+Opt+$/", $v->name)){
+    	// 	if(preg_match("/^radio+[0-9]+$/", $value->name)){ //radio
+    	// 		foreach ($request->form as $k => $v) {
+    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
 
-    					$form[] = [$value->value=>$v->value];
-    				}
-    			}
-    		}
-    		elseif(preg_match("/^multi+[0-9]+$/", $value->name)){ //multi
-    			foreach ($request->form as $k => $v) {
-    				if(preg_match("/^$value->name+Opt+$/", $v->name)){
+    	// 				$form[] = [$value->value=>$v->value];
+    	// 			}
+    	// 		}
+    	// 	}
+    	// 	elseif(preg_match("/^select+[0-9]+$/", $value->name)){ //select
+    	// 		foreach ($request->form as $k => $v) {
+    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
 
-    					$form[] = [$value->value=>$v->value];
-    				}
-    			}
-    		}
-    		elseif(preg_match("/^qa+[0-9]+$/", $value->name)){ //qa
-    			foreach ($request->form as $k => $v) {
-    				if(preg_match("/^$value->name+Opt+$/", $v->name)){
+    	// 				$form[] = [$value->value=>$v->value];
+    	// 			}
+    	// 		}
+    	// 	}
+    	// 	elseif(preg_match("/^multi+[0-9]+$/", $value->name)){ //multi
+    	// 		foreach ($request->form as $k => $v) {
+    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
 
-    					$form[] = [$value->value=>$v->value];
-    				}
-    			}
-    		}
-    		elseif(preg_match("/^part+[0-9]+$/", $value->name)){ //part
-    			foreach ($request->form as $k => $v) {
-    				if(preg_match("/^$value->name+Opt+$/", $v->name)){
+    	// 				$form[] = [$value->value=>$v->value];
+    	// 			}
+    	// 		}
+    	// 	}
+    	// 	elseif(preg_match("/^qa+[0-9]+$/", $value->name)){ //qa
+    	// 		foreach ($request->form as $k => $v) {
+    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
 
-    					$form[] = [$value->value=>$v->value];
-    				}
-    			}
-    		}
-    		elseif(preg_match("/^date+[0-9]+$/", $value->name)){ //date
-    			foreach ($request->form as $k => $v) {
-    				if(preg_match("/^$value->name+Opt+$/", $v->name)){
+    	// 				$form[] = [$value->value=>$v->value];
+    	// 			}
+    	// 		}
+    	// 	}
+    	// 	elseif(preg_match("/^part+[0-9]+$/", $value->name)){ //part
+    	// 		foreach ($request->form as $k => $v) {
+    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
 
-    					$form[] = [$value->value=>$v->value];
-    				}
-    			}
-    		}
-    		elseif(preg_match("/^time+[0-9]+$/", $value->name)){ //time
-    			foreach ($request->form as $k => $v) {
-    				if(preg_match("/^$value->name+Opt+$/", $v->name)){
+    	// 				$form[] = [$value->value=>$v->value];
+    	// 			}
+    	// 		}
+    	// 	}
+    	// 	elseif(preg_match("/^date+[0-9]+$/", $value->name)){ //date
+    	// 		foreach ($request->form as $k => $v) {
+    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
 
-    					$form[] = [$value->value=>$v->value];
-    				}
-    			}
-    		}
-    	}
+    	// 				$form[] = [$value->value=>$v->value];
+    	// 			}
+    	// 		}
+    	// 	}
+    	// 	elseif(preg_match("/^time+[0-9]+$/", $value->name)){ //time
+    	// 		foreach ($request->form as $k => $v) {
+    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
+
+    	// 				$form[] = [$value->value=>$v->value];
+    	// 			}
+    	// 		}
+    	// 	}
+    	// }
 
     	// $id = Account::where('token',$request->token)->get();
 

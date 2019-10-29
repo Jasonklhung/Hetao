3c989f80de90a647ee75b190110f969595b110e6
JasonHung <jasonklhung@accuhit.net>
2019-10-29 19:55:48 +0800

    test

--- app/Http/Controllers/API/SatisfactionController.php
+++ app/Http/Controllers/API/SatisfactionController.php
@@ -21,74 +21,18 @@ class SatisfactionController extends Controller
     {
     	$form = array();
 
-    	// foreach ($request->form as $key => $value) {
+    	foreach ($request->form as $key => $value) {
 
-    	// 	if(preg_match("/^radio+[0-9]+$/", $value->name)){ //radio
-    	// 		foreach ($request->form as $k => $v) {
-    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
+             $form[$key] = $value;
+        }
 
-    	// 				$form[] = [$value->value=>$v->value];
-    	// 			}
-    	// 		}
-    	// 	}
-    	// 	elseif(preg_match("/^select+[0-9]+$/", $value->name)){ //select
-    	// 		foreach ($request->form as $k => $v) {
-    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
+    	$id = Account::where('token',$request->token)->get();
 
-    	// 				$form[] = [$value->value=>$v->value];
-    	// 			}
-    	// 		}
-    	// 	}
-    	// 	elseif(preg_match("/^multi+[0-9]+$/", $value->name)){ //multi
-    	// 		foreach ($request->form as $k => $v) {
-    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
+    	$res = new SatisfactionAnswer;
+    	$res->account_id = $id[0]['id'];
+    	$res->form = json_encode($form);
+    	$res->save();   
 
-    	// 				$form[] = [$value->value=>$v->value];
-    	// 			}
-    	// 		}
-    	// 	}
-    	// 	elseif(preg_match("/^qa+[0-9]+$/", $value->name)){ //qa
-    	// 		foreach ($request->form as $k => $v) {
-    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
-
-    	// 				$form[] = [$value->value=>$v->value];
-    	// 			}
-    	// 		}
-    	// 	}
-    	// 	elseif(preg_match("/^part+[0-9]+$/", $value->name)){ //part
-    	// 		foreach ($request->form as $k => $v) {
-    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
-
-    	// 				$form[] = [$value->value=>$v->value];
-    	// 			}
-    	// 		}
-    	// 	}
-    	// 	elseif(preg_match("/^date+[0-9]+$/", $value->name)){ //date
-    	// 		foreach ($request->form as $k => $v) {
-    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
-
-    	// 				$form[] = [$value->value=>$v->value];
-    	// 			}
-    	// 		}
-    	// 	}
-    	// 	elseif(preg_match("/^time+[0-9]+$/", $value->name)){ //time
-    	// 		foreach ($request->form as $k => $v) {
-    	// 			if(preg_match("/^$value->name+Opt+$/", $v->name)){
-
-    	// 				$form[] = [$value->value=>$v->value];
-    	// 			}
-    	// 		}
-    	// 	}
-    	// }
-
-
-    	// $id = Account::where('token',$request->token)->get();
-
-    	// $res = new SatisfactionAnswer;
-    	// $res->account_id = $id[0]['id'];
-    	// $res->form = json_encode($form);
-    	// $res->save();   
-
-    	dd($request->form);
+    	return 'ok';
     }
 }

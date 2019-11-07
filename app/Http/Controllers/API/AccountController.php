2696b5480354f0a50dc6da666ee635672796ea4e
JasonHung <jasonklhung@accuhit.net>
2019-11-07 15:21:11 +0800

    line api 更換客戶選單

--- app/Http/Controllers/API/AccountController.php
+++ app/Http/Controllers/API/AccountController.php
@@ -12,6 +12,15 @@ class AccountController extends Controller
 {
     public function store(Request $request)
     {
+        $richmenuId = 'richmenu-b091aa3f171ce930505b87d8bbf00873' ;
+        $channel = 'kigQJsG6rQh2yJFhqcpQY0WMc/xSsUFLFwuoTs+N4zo0Xx7BmN+qxxXZ0m2IXSb31++yliJDSvyIeLYci3ZrOIHus58KTjVQrLydr2+fk6q+2TmnPThJUzcDtoaXy15KdbHuqdXkhhKM/oJ/33qLiAdB04t89/1O/w1cDnyilFU=' ;
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->post('https://api.line.me/v2/bot/user/'.$request->userId.'/richmenu/'.$richmenuId, [
+            'headers' => ['Content-Length' => '0','Authorization' => 'Bearer '.$channel],
+        ]);
+
+
         $depart = Department::where('name',$request->DEPT)->get();
 
     	$account = new Account;

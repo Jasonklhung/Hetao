8c5ba074a946defb7a78bc7f59a7435900ddf781
JasonHung <jasonklhung@accuhit.net>
2019-11-13 11:25:03 +0800

    修改欄位

--- app/Http/Controllers/API/AccountController.php
+++ app/Http/Controllers/API/AccountController.php
@@ -28,7 +28,6 @@ class AccountController extends Controller
         $account->department_id = $depart[0]['id'];
     	$account->token = $request->userId;
     	$account->cuskey = $request->CUSTKEY;
-    	$account->dept = $request->DEPT;
     	$account->name = $request->name;
     	$account->card_number = $request->CARDNO;
     	$account->save();

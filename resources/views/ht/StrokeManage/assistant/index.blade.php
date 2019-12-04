cb1ba8da54022b0045042368aa05b12a1b4738af
peihsuan <peggy@accuhit.net>
2019-12-04 15:21:14 +0800

    style modify

--- resources/views/ht/StrokeManage/assistant/index.blade.php
+++ resources/views/ht/StrokeManage/assistant/index.blade.php
@@ -1194,7 +1194,7 @@
                     "responsive": {
                         "breakpoints": [
                         { name: 'desktop', width: Infinity},
-                        { name: 'tablet',  width: 1007},
+                        { name: 'tablet',  width: 1024},
                         ],
                         "details": {
                             "display": $.fn.dataTable.Responsive.display.childRowImmediate,

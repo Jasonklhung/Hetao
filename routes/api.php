433fc31de260a192fcfe22685354fdd16baf9d18
JasonHung <jasonklhung@accuhit.net>
2019-12-17 17:09:59 +0800

    get -> post

--- routes/api.php
+++ routes/api.php
@@ -24,16 +24,16 @@ use Illuminate\Http\Request;
 //表單
 Route::get('getResData','Api\ReservationController@index');
 Route::post('resStore','Api\ReservationController@store');
-Route::get('reservation','Api\ReservationController@show');
+Route::post('reservation','Api\ReservationController@show');
 Route::post('statusUpdate','Api\ReservationController@update');
 
 Route::get('getContactData','Api\ContactController@index');
 Route::post('contactStore','Api\ContactController@store');
-Route::get('contact','Api\ContactController@show');
+Route::post('contact','Api\ContactController@show');
 
 Route::get('getSatiData','Api\SatisfactionController@index');
 Route::post('satiStore','Api\SatisfactionController@store');
-Route::get('satisfaction','Api\SatisfactionController@show');
+Route::post('satisfaction','Api\SatisfactionController@show');
 
 //account
 Route::post('accountStore','Api\AccountController@store');

46cbbb1fcaa742d94d092598ad35f961388a8e66
JasonHung <jasonklhung@accuhit.net>
2020-03-12 15:29:05 +0800

    領退料明細API

--- app/Http/Controllers/API/MaterialController.php
+++ app/Http/Controllers/API/MaterialController.php
@@ -24,34 +24,39 @@ class MaterialController extends Controller
     	elseif(empty($date) && !empty($dept)){
 
     		$org = Organization::where('name',$dept)->get();
-    		$org_id = $org->id;
-    		$receive = Material::where('organization_id',$org_id);
-    		$back = MaterialBack::where('organization_id',$org_id);
+    		$org_id = $org[0]['id'];
+    		$receive = Material::where('organization_id',$org_id)->get();
+    		$back = MaterialBack::where('organization_id',$org_id)->get();
     	}
     	elseif(!empty($date) && empty($dept)){
 
-    		$receive = Material::whereDate('date','=',$date);
-    		$back = MaterialBack::whereDate('date','=',$date);
+    		$receive = Material::whereDate('date','=',$date)->get();
+    		$back = MaterialBack::whereDate('date','=',$date)->get();
     	}
     	else{
 
     		$org = Organization::where('name',$dept)->get();
-    		$org_id = $org->id;
-    		$receive = Material::where('organization_id',$org_id)->whereDate('date','=',$date);
-    		$back = MaterialBack::where('organization_id',$org_id)->whereDate('date','=',$date);
+    		$org_id = $org[0]['id'];
+    		$receive = Material::where('organization_id',$org_id)->whereDate('date','=',$date)->get();
+    		$back = MaterialBack::where('organization_id',$org_id)->whereDate('date','=',$date)->get();
     	}
 
-    	$material = array();
-
-    	foreach ($receive as $key => $value) {
-    		$material["receive"][] = array("date"=>$value->date,"emp_id"=>$value->emp_id,"emp_name"=>$value->emp_name,"materials_number"=>$value->materials_number,"materials_spec"=>$value->materials_spec,"machine_number"=>$value->machine_number,"quantity"=>$value->quantity,"other"=>$value->other);
+    	if($receive->isEmpty() && $back->isEmpty()){
+    		return json_encode(array("status" => 400 , "message" => "沒有符合的資料"));
     	}
+    	else{
+    		$material = array();
 
-    	foreach ($back as $k => $v) {
-    		$material["back"][] = array("date"=>$v->date,"emp_id"=>$v->emp_id,"emp_name"=>$v->emp_name,"materials_number"=>$v->materials_number,"materials_spec"=>$v->materials_spec,"machine_number"=>$v->machine_number,"quantity"=>$v->quantity,"other"=>$v->other);
+    		foreach ($receive as $key => $value) {
+    			$material["receive"][] = array("date"=>$value->date,"emp_id"=>$value->emp_id,"emp_name"=>$value->emp_name,"materials_number"=>$value->materials_number,"materials_spec"=>$value->materials_spec,"machine_number"=>$value->machine_number,"quantity"=>$value->quantity,"other"=>$value->other);
+    		}
+
+    		foreach ($back as $k => $v) {
+    			$material["back"][] = array("date"=>$v->date,"emp_id"=>$v->emp_id,"emp_name"=>$v->emp_name,"materials_number"=>$v->materials_number,"materials_spec"=>$v->materials_spec,"machine_number"=>$v->machine_number,"quantity"=>$v->quantity,"other"=>$v->other);
+    		}
+
+    		return $material;
     	}
-
-    	return $material;
     }
 
     public function get_material()

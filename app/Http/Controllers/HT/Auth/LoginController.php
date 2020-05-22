209a88a8e65e31982983ab0f47b14895b094b96c
JasonHung <jasonklhung@accuhit.net>
2020-05-22 18:45:37 +0800

    修改redirect route

--- app/Http/Controllers/HT/Auth/LoginController.php
+++ app/Http/Controllers/HT/Auth/LoginController.php
@@ -217,6 +217,8 @@ class LoginController extends Controller
 
             if (Auth::attempt(array('mobile' => $user['mobile'], 'password' => $user['emp_id']))){
 
+                $organization = $user['organization_id'];
+
                 if($route == 'overview'){
                     return redirect()->route('ht.Overview.index',compact('organization'));
                 }
